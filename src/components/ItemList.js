66a8f9dbcd1f9ecb85bac3eda20bbfdc0c36205f
Ansel <anselcolvin@gmail.com>
2017-12-09 23:33:18 -0500

    Remove shortid and change keys of ListItems to be contents

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,8 +5,8 @@ class ItemList extends React.Component {
     render() {
         return (
             <div className='item-list'>
-                {this.props.list.map(function (listValue, index) {
-                    return <ListItem key={ index } contents={ listValue } />
+                {this.props.list.map(function (listValue) {
+                    return <ListItem key={ listValue } contents={ listValue } />
                 })}
             </div>
         )
